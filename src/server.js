c5735337647f216d07d7d91b9fa9bd46b6200ca7
Shaik Sajid Hussain <sanjusazid0@gmail.com>
2026-05-12 00:45:10 +0530

    Updated the cors

--- src/server.js
+++ src/server.js
@@ -7,11 +7,15 @@ app.use(
 cors({
 origin: [
 'http://localhost:5173',
-'https://milk-diary-fe.vercel.app'
+'https://milk-diary-fe.vercel.app',
 ],
+methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+allowedHeaders: ['Content-Type', 'Authorization'],
 credentials: true,
 })
 )
+
+app.options('*', cors())
 
 app.use(express.json())
 
